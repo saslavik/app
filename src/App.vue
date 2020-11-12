bfd78f6b60212a4a98c4b80e3126f869a398baa4
saslavik <saslavik@gmail.com>
2020-11-12 15:27:39 +0200

    components added

--- src/App.vue
+++ src/App.vue
@@ -1,52 +1,6 @@
 <template>
   <section class="catalog">
-    <ul class="catalog__list">
-      <li class="catalog__item" v-for="(product, index) in products" :key="index">
-        <a class="catalog__pic" href="#">
-          <img :src="product.image" :alt="product.title">
-        </a>
-
-        <h3 class="catalog__title">
-          <a href="#">
-            {{ product.title }}
-          </a>
-        </h3>
-
-        <span class="catalog__price">
-          {{ product.price }} ₽
-        </span>
-
-        <ul class="colors colors--black">
-          <li class="colors__item">
-            <label class="colors__label">
-              <input
-              class="colors__radio sr-only"
-              type="radio"
-              name="color-1"
-              value="#73B6EA"
-              checked="">
-              <span class="colors__value" style="background-color: #73B6EA;">
-              </span>
-            </label>
-          </li>
-          <li class="colors__item">
-            <label class="colors__label">
-              <input class="colors__radio sr-only" type="radio" name="color-1" value="#8BE000">
-              <span class="colors__value" style="background-color: #8BE000;">
-              </span>
-            </label>
-          </li>
-          <li class="colors__item">
-            <label class="colors__label">
-              <input class="colors__radio sr-only" type="radio" name="color-1" value="#222">
-              <span class="colors__value" style="background-color: #222;">
-              </span>
-            </label>
-          </li>
-        </ul>
-      </li>
-    </ul>
-
+    <productList :products="products" />
     <ul class="catalog__pagination pagination">
       <li class="pagination__item">
         <a
@@ -103,9 +57,13 @@
 
 <script>
 import products from './data/products';
+import productList from './components/productList.vue';
 
 export default {
   name: 'App',
+  components: {
+    productList,
+  },
   data() {
     return {
       products,

31f70e5fe51d0e39ecf0223d100c93950af35492
saslavik <saslavik@gmail.com>
2020-11-28 20:52:36 +0200

    product add to cart

--- src/store/index.js
+++ src/store/index.js
@@ -9,4 +9,17 @@ export default new Vuex.Store({
       { productId: 1, amount: 2 },
     ],
   },
+  mutations: {
+    addProductToCart(state, { productId, amount }) {
+      const item = state.cartProducts.find((el) => el.productId === productId);
+      if (item) {
+        item.amount += amount;
+      } else {
+        state.cartProducts.push({
+          productId,
+          amount,
+        });
+      }
+    },
+  },
 });

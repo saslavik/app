065fa4bbf8d499dc4babc99c9f17c8143bf1d179
saslavik <saslavik@gmail.com>
2020-12-10 12:59:14 +0200

    workout

--- src/pages/CartPages.vue
+++ src/pages/CartPages.vue
@@ -38,9 +38,10 @@
             Итого: <span>{{ totalPrice | numberFormat }} ₽</span>
           </p>
 
-          <button class="cart__button button button--primery" type="submit">
+          <router-link tag="button" :to="{name: 'order'}"
+          class="cart__button button button--primery">
             Оформить заказ
-          </button>
+          </router-link>
         </div>
       </form>
     </section>
--- src/router/index.js
+++ src/router/index.js
@@ -4,12 +4,14 @@ import MainPage from '@/pages/MainPage.vue';
 import NotFoundPage from '@/pages/NotFoundPage.vue';
 import ProductPage from '@/pages/ProductPage.vue';
 import CartPages from '@/pages/CartPages.vue';
+import OrderPage from '@/pages/OrderPage.vue';
 
 Vue.use(VueRouter);
 
 const routes = [{ name: 'main', component: MainPage, path: '/' },
   { name: 'product', component: ProductPage, path: '/products/:id' },
   { name: 'cart', component: CartPages, path: '/cart' },
+  { name: 'order', component: OrderPage, path: '/order' },
   { name: 'notFound', component: NotFoundPage, path: '*' }];
 
 const router = new VueRouter({
